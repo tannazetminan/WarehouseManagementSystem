c70c6dfc20ca4640d7f05e6a933f110a5078cfc8
ElizabethRamdeo <ramdeoe@student.douglascollege.ca>
2024-11-12 14:06:27 -0800

    Added two classes one for product and one for transaction

--- app/src/main/java/com/example/warehousemanagementsystem/Product.kt
+++ app/src/main/java/com/example/warehousemanagementsystem/Product.kt
@@ -5,6 +5,7 @@ data class Product(
     val prodID: Int,
     val prodName: String,
     val prodDescription: String? = null,
+    val prodCategory: String? = null,
     val price: Double,
     val quantity: Int? = null,
     val image_url: String? =null,
