a894892bf4eb646cf95c4cc19f5ab838ec81de8c
ElizabethRamdeo <ramdeoe@student.douglascollege.ca>
2024-11-12 14:06:39 -0800

    Added two classes one for product and one for transaction

--- app/src/main/java/com/example/warehousemanagementsystem/Transaction.kt
+++ app/src/main/java/com/example/warehousemanagementsystem/Transaction.kt
@@ -4,6 +4,7 @@ data class Transaction(
     val transId: Int,
     val userId: Int, //refers to the customer or admin user is who performed the transaction
     val transDate: String, //date of the transaction
-    //how to handle items with prod it, name, quantity and price per item?
+    val transTime: String, //time of transaction
+    var products: Array<Product> = emptyArray(),
     val transTotal: Double
 )
