f09fcba2c58714b024b0477aeb8109cd3a9d6edb
300370605 <ramdeoe@student.douglascollege.ca>
2024-11-20 15:06:36 -0800

    Added button for user management on admin page, and did card layout for inventory

--- app/src/main/java/com/example/warehousemanagementsystem/Transaction.kt
+++ app/src/main/java/com/example/warehousemanagementsystem/Transaction.kt
@@ -5,9 +5,10 @@ data class Transaction(
     val transId: Int,
     //changed this to User type
     val user: User, //refers to the customer or admin user is who performed the transaction
-    val transDate: String, //date of the transaction
-    val transTime: String, //time of transaction
+    val transDateAndTime: String, //date of the transaction
+    //val transTime: String, //time of transaction
     var products: Array<Product> = emptyArray(),
+
     val transTotal: Double = 0.0, // This could be calculated dynamically, no need to store it directly in the DB
     //removed "transProfit" for now as this shall be a calculated value derived from "salePrice" and "costPrice" of the products in the array, same for "transTotal"
     //should we also ass a field for transaction category i.e. supplyrequest or purchase
